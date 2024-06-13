1bcdc6af308c301f613c5cac1f091f38983ceab3
GiovanniCristian <marroccogc@gmail.com>
2024-06-13 14:31:25 +0200

    fix: layout modal fixed

--- src/components/modals/recipeModal/RecipeModal.tsx
+++ src/components/modals/recipeModal/RecipeModal.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Modal, Button, Typography, Carousel } from 'antd';
+import { Modal, Button, Typography, Carousel, Rate } from 'antd';
 import { RecipesModalProps } from '../../../interfaces/recipes';
 import CommentModal from '../commentModal/CommentModal';
 import './recipeModal.css'
@@ -20,29 +20,31 @@ const RecipeModal: React.FC<RecipesModalProps> = ({ open, onClose, recipe, addCo
             footer={null}
             width={700}
             centered
-            style={{padding: '1rem'}}
+            style={{ padding: '1rem' }}
         >
             <Title className='title' level={2}>{recipe.name}</Title>
             <img src={`http://localhost:8080${recipe.image}`} alt={recipe.name} className="recipe-image" />
-            <Title className='title' level={4}>Ingredients:</Title>
+            <Title className='ingredients-title' level={5}>Ingredients:</Title>
             <div className="recipe-ingredients">
                 {recipe.ingredients.map((ingredient, index) => (
                     <span key={index} className="ingredient">{ingredient}</span>
                 ))}
             </div>
-            <Title className='title' level={4}>Instructions:</Title>
+            <Title className='title' level={5}>Instructions:</Title>
             <div className="recipe-instructions">{recipe.instructions}</div>
-            <Title className='title' level={4}>Cuisine: {recipe.cuisineId}</Title>
-            <Title className='title' level={4}>Diet: {recipe.dietId}</Title>
-            <Title className='title' level={4}>Difficulty: {recipe.difficultyId}</Title>
-            <Title className='title' level={4}>Comments:</Title>
+            <div className='recipe-specifics'>
+                <Title className='spec-title' level={5}>&#129368; {recipe.cuisineId}</Title>
+                <Title className='spec-title' level={5}>&#129388; {recipe.dietId} &#129388;</Title>
+                <Title className='spec-title' level={5}>&#128246; {recipe.difficultyId}</Title>
+            </div>
+            <Title className='comments' level={5}>Comments:</Title>
             <Carousel autoplay className="comment-carousel">
                 {recipe.comments.map((comment, index) => (
                     <div key={index} className="comment-slide">
                         <div>
-                            <strong>Rating: {comment.rating}</strong>
-                            <p>{comment.comment}</p>
-                            <small>{new Date(comment.date).toLocaleString()}</small>
+                            <Rate value={comment.rating} disabled defaultValue={comment.rating} />
+                            <p className='comment-text'>{comment.comment}</p>
+                            <small className='comment-date'>{new Date(comment.date).toLocaleString()}</small>
                         </div>
                     </div>
                 ))}
